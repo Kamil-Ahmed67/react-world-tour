9c168ba78eac1adafa14eb9272815353913a1d29
Kamil-Ahmed67 <nabilahmedtalukder6272@gmail.com>
2024-10-20 01:26:51 +0600

    Toggle Button Added

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,22 +1,25 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css'
 
 const Countries = () => {
     //use state to hold the data
-    const [countries,setCountries]= useState([]);
+    const [countries, setCountries] = useState([]);
     //use effect to get the data
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(response=>response.json())
-        .then(data=> setCountries(data))
-    },[])
+            .then(response => response.json())
+            .then(data => setCountries(data))
+    }, [])
     return (
         <div>
             <h3>Countries:{countries.length}</h3>
-            {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
-            }
+            <div className="countries-container">
+                {
+                    countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
